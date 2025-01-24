e6d238ecd44d9e4b644ae20b3870f5f6684109fe
Igor Barbosa <igor.barbosa.dev2@gmail.com>
2025-01-24 09:59:58 -0300

    Task-02: Creating connection to database

---
+++ src/database.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import { Producer } from './app/producer.entity';  
+
+export const dataSource = new DataSource({
+  type: 'sqlite',
+  database: ':memory:',  
+  synchronize: true,      
+  entities: [Producer],   
+});
+
+export const createDatabaseConnection = async () => {
+  try {
+    await dataSource.initialize(); 
+    console.log('Connection to database successful!');
+  } catch (error) {
+    console.error('Error connecting to database', error);
+  }
+};
