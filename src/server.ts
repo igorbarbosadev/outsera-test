2d7356dc8166d0d46f73b5a36f23ea79cec7deeb
Igor Barbosa <igor.barbosa.dev2@gmail.com>
2025-01-24 10:12:03 -0300

    Task-05: Implementing connection to database and import data

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from 'express';
 import routes from './routes';
+import { createDatabaseConnection } from './database';
+import importProducers from './functions/importProducers';
 
 const app = express();
 
-app.use(express.json());
+const startServer = async () => {
+    try {
+        await createDatabaseConnection();
+        await importProducers();
+        
+        app.use(express.json());
+        app.use('/',routes);      
+        app.listen(5000, () => console.log(`Server listening on port 5000`));
 
-app.use('/',routes)
+    } catch (error) {
+        console.error('Erro ao iniciar o servidor', error);
+    }
+};
 
-app.listen(5000, () => console.log(`Server listening on port 5000`));
+startServer();
