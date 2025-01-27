224d1351516900d649cee1ab555d77e86182ca1a
Igor Barbosa <igor.barbosa.dev2@gmail.com>
2025-01-27 10:49:29 -0300

    Task-19/refactor: refactoring integration test to validate only the expected response body

--- src/test/http/HttpGetProducersAwardIntervals.test.ts
+++ src/test/http/HttpGetProducersAwardIntervals.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import {app} from'../../server';
 import { createDatabaseConnection, dataSource } from '../../database';
 import { Producer } from '../../app/producer.entity';
+import importProducers from '../../functions/importProducers';
 
 describe('GET /api/v1/producers/award-intervals', () => {
   
@@ -11,27 +12,29 @@ describe('GET /api/v1/producers/award-intervals', () => {
 
   it('should return the correct min and max award intervals for producers', async () => {
     
-    const producers = 'Producer 1';
-    const winner = 'yes';
-    const studios = 'studio 1';
+    await importProducers();
 
-    const producerRepository = dataSource.getRepository(Producer);
-
-    await producerRepository.save([
-      { producers, winner, studios, title: 'Title 1', year: 2000 },
-      { producers, winner, studios, title: 'Title 2', year: 2005 },
-      { producers, winner, studios, title: 'Title 3', year: 2001 },
-      { producers, winner, studios, title: 'Title 4', year: 2004 },
-    ]);
-
-    // Act: Fazer uma requisição GET para o endpoint
     const response = await request(app).get('/api/v1/producers/award-intervals');
 
-    // Assert: Validar a resposta
-    expect(response.status).toBe(200); // Verifica se o status da resposta é 200 (OK)
-    expect(response.body.min).toHaveLength(2); // Verifica se há dois intervalos mínimos
-    expect(response.body.max).toHaveLength(2); // Verifica se há dois intervalos máximos
-    expect(response.body.min[0].interval).toBe(1); // Verifica se o intervalo mínimo está correto
-    expect(response.body.max[0].interval).toBe(3); // Verifica se o intervalo máximo está correto
+    expect(response.status).toBe(200);
+    
+    expect(response.body).toEqual({
+      "min": [
+        {
+          "producer": "Joel Silver",
+          "interval": 1,
+          "previousWin": 1990,
+          "followingWin": 1991
+        }
+      ],
+      "max": [
+        {
+          "producer": "Matthew Vaughn",
+          "interval": 13,
+          "previousWin": 2002,
+          "followingWin": 2015
+        }
+      ]
+    })
   });
 });
